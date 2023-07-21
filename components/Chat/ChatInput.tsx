dc829e25676afee886339e4c7608b36dfdcdba35
rc1 <ross@electricglen.com>
2023-07-21 20:36:27 +0100

    Added tile

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -386,7 +386,7 @@ export const ChatInput = ({
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Martin 🤖
         </a>
         .{' '}
         {t(
